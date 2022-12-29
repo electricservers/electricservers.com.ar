ef629408a0108d75b9e3b2e8960f49177ba0512b
Maximiliano Jabase <maxijabase@gmail.com>
2022-12-29 15:08:34 -0300

    remove gamedig

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,24 +1,24 @@
-import Gamedig from 'gamedig'
 import type { ServerInfo } from 'src/types/serverInfo'
+import pkg from '@fabricio-191/valve-server-query'
+const { Server } = pkg;
 
 import { serverList } from '../servers/serversList'
 import type { LayoutServerLoad } from './$types'
 
 export const load = (async () => {
   let infoList: ServerInfo[] = []
-  for await (const server of serverList) {
-    await Gamedig.query({
-      type: 'tf2',
-      host: server.ip,
+  for (const server of serverList) {
+    const query = await Server({
+      ip: server.ip,
       port: server.port,
-    }).then(state => {
-      infoList.push({
-        connect: state.connect,
-        name: state.name,
-        players: state.players.length,
-        maxplayers: state.maxplayers,
-        map: state.map,
-      })
+    })
+    const svInfo = await query.getInfo()
+    infoList.push({
+      connect: svInfo.address + ':' + svInfo.port,
+      name: svInfo.name,
+      map: svInfo.map,
+      maxplayers: svInfo.players.max,
+      players: svInfo.players.online,
     })
   }
   return {
