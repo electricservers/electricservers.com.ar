083abe4a5563f99f3c0af7e87416462458051b6b
Maximiliano Jabase <maxijabase@gmail.com>
2022-12-29 16:24:37 -0300

    use preprocess import assets pkg to fix images

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
 import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/kit/vite'
+import importAssets from 'svelte-preprocess-import-assets'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: [importAssets()],
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
