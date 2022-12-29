9856de8a64dcd8ee8e8df60af25c24930964dcd2
Maximiliano Jabase <maxijabase@gmail.com>
2022-12-29 16:28:08 -0300

    some adjustments to server fetching

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,25 +1,30 @@
 import type { ServerInfo } from 'src/types/serverInfo'
 import pkg from '@fabricio-191/valve-server-query'
-const { Server } = pkg;
+const { Server } = pkg
 
 import { serverList } from '../servers/serversList'
 import type { LayoutServerLoad } from './$types'
 
 export const load = (async () => {
   let infoList: ServerInfo[] = []
-  for (const server of serverList) {
-    const query = await Server({
-      ip: server.ip,
-      port: server.port,
-    })
-    const svInfo = await query.getInfo()
-    infoList.push({
-      connect: svInfo.address,
-      name: svInfo.name,
-      map: svInfo.map,
-      maxplayers: svInfo.players.max,
-      players: svInfo.players.online,
-    })
+  for await (const server of serverList) {
+    try {
+      const query = await Server({
+        ip: server.ip,
+        port: server.port,
+        timeout: 4000,
+      })
+      const svInfo = await query.getInfo()
+      infoList.push({
+        connect: svInfo.address,
+        name: svInfo.name,
+        map: svInfo.map,
+        maxplayers: svInfo.players.max,
+        players: svInfo.players.online,
+      })
+    } catch {
+      continue
+    }
   }
   return {
     serverList: infoList,
