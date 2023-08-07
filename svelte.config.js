a32bfa38ff8d60804e44ffdcf6d8de95c5708d2e
Maximiliano Jabase <maxijabase@gmail.com>
2023-08-07 01:38:51 -0300

    ncu -u

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/kit/vite'
-import importAssets from 'svelte-preprocess-import-assets'
+import { importAssets } from 'svelte-preprocess-import-assets'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
